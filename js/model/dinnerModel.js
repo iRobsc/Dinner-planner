0aeaa37dda120df89e3ad41252cb02e7c5c677d6
Willy Liu <arkazain@gmail.com>
2018-01-19 16:06:11 +0100

    ✅ Implement model

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -2,52 +2,71 @@
 const DinnerModel = () => { // eslint-disable-line
   // TODO Lab 1 implement the data structure that will hold number of guest
   // and selected dishes for the dinner menu
-
+  this.numberOfGuests = 0;
+  this.menu = [];
 
   this.setNumberOfGuests = (num) => {
-    // TODO Lab 1
+    this.numberOfGuests = num;
   };
 
-  this.getNumberOfGuests = () => {
-    // TODO Lab 1
-  };
+  this.getNumberOfGuests = () => this.numberOfGuests;
 
   // Returns the dish that is on the menu for selected type
   this.getSelectedDish = (type) => {
-    // TODO Lab 1
+    const res = [];
+    for (let i = 0; i < this.menu.length; i++) {
+      if (this.menu[i].type === type) {
+        res.push(this.menu[i]);
+      }
+    }
+    return res;
   };
 
   // Returns all the dishes on the menu.
-  this.getFullMenu = () => {
-    // TODO Lab 1
-  };
+  this.getFullMenu = () => this.menu;
 
   // Returns all ingredients for all the dishes on the menu.
   this.getAllIngredients = () => {
-    // TODO Lab 1
+    let res = [];
+    for (let i = 0; i < this.menu.length; i++) {
+      res = res.concat(this.menu[i].ingredients);
+    }
+    return res;
   };
 
   // Returns the total price of the menu (all the ingredients multiplied by number of guests).
   this.getTotalMenuPrice = () => {
-    // TODO Lab 1
+    let price = 0;
+    const allIngredients = this.getAllIngredients();
+    for (let i = 0; i < allIngredients.length; i++) {
+      const current = allIngredients[i];
+      price += current.price;
+    }
+    return price * this.numberOfGuests;
   };
 
   // Adds the passed dish to the menu. If the dish of that type already exists on the menu
   // it is removed from the menu and the new one added.
   this.addDishToMenu = (id) => {
-    // TODO Lab 1
+    this.removeDishFromMenu(id);
+    this.menu.push(this.getDish(id));
   };
 
   // Removes dish from menu
   this.removeDishFromMenu = (id) => {
-    // TODO Lab 1
+    for (let i = 0; i < this.menu.length; i++) {
+      if (this.menu[i].id === id) {
+        this.menu.splice(i, 1);
+        return;
+      }
+    }
   };
 
   // function that returns all dishes of specific type (i.e. "starter", "main dish" or "dessert")
   // you can use the filter argument to filter out the dish by name or ingredient (use for search)
-  // if you don"t pass any filter all the dishes will be returned
-  this.getAllDishes = ((type, filter) => {
-    return dishes.filter((dish) => {
+  // if you don't pass any filter all the dishes will be returned
+  this.getAllDishes = ((type, filter) =>
+    dishes.filter((dish) => {
       let found = true;
       if (filter) {
         found = false;
@@ -61,8 +80,8 @@ const DinnerModel = () => { // eslint-disable-line
         }
       }
       return dish.type === type && found;
-    });
-  });
+    })
+  );
 
   // function that returns a dish of specific ID
   this.getDish = (id) => {
@@ -71,6 +90,7 @@ const DinnerModel = () => { // eslint-disable-line
         return dishes[key];
       }
     }
+    return -1;
   };
 
 
