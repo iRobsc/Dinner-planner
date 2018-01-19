e7a8c1ac9aaf2e32e6dee3894ee8bc2b4e8b34c5
Willy Liu <arkazain@gmail.com>
2018-01-19 13:41:30 +0100

    🚧 Set up npm with eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+        "node": false,
+        "browser": true,
+    },
+    "rules": {
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "no-console": 0,
+        "no-underscore-dangle": 0,
+        "no-use-before-define": 0,
+        "no-plusplus": 0,
+    },
+};
