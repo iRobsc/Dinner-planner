17ddd33a7918fdaa961ece2ffab2d8de5fbdc6c9
Willy Liu <arkazain@gmail.com>
2018-01-23 23:18:39 +0100

    🚧 Refactor Sidebar class

--- js/view/sidebar.js
+++ js/view/sidebar.js
@@ -1,15 +1,25 @@
 class Sidebar {
   constructor(container, model) {
-    const menuBtn = container.querySelector("#sidebar-accordion");
-    const sidebarContent = container.querySelector("#sidebar-content");
+    this.container = container;
+    this.model = model;
+
+    this.menuBtn();
+    this.numberOfGuests();
+  }
+
+  menuBtn() {
+    const menuBtn = this.container.querySelector("#sidebar-accordion");
+    const sidebarContent = this.container.querySelector("#sidebar-content");
 
     menuBtn.addEventListener("click", () => {
       sidebarContent.classList.toggle("sidebar-hide");
       sidebarContent.classList.toggle("sidebar-show");
     });
+  }
 
-    const numberOfGuests = model.getNumberOfGuests();
-    const guestElem = container.querySelector("#number-of-guests");
+  numberOfGuests() {
+    const numberOfGuests = this.model.getNumberOfGuests();
+    const guestElem = this.container.querySelector("#number-of-guests");
     guestElem.textContent = numberOfGuests;
   }
 }
