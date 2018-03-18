f541c13016d6948f3cbc17400a066e19103e60fa
Robert Kindwall <kindwall@kth.se>
2018-03-18 17:00:00 +0100

    Add Welcome component and remove onClick for DinnerButtonLink

--- src/components/DinnerButtonLink.js
+++ src/components/DinnerButtonLink.js
@@ -2,16 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./../css/buttons.scss";
 
-function DinnerButtonLink({ to, children, onClick }) {
+function DinnerButtonLink({ to, children }) {
   return (
-    <a className="dinner-btn" href={to} onClick={onClick}>{children}</a>
+    <a className="dinner-btn" href={to}>{children}</a>
   );
 }
 
 DinnerButtonLink.propTypes = {
   to: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
-  onClick: PropTypes.func.isRequired,
 };
 
 export default DinnerButtonLink;
