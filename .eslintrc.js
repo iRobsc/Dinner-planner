15174f6b7b66b2bcfe96f909458662c29c34ca8a
Willy Liu <arkazain@gmail.com>
2018-03-19 20:22:00 +0100

    ⚙️ Update eslint config to not throw error on react-router Links

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,10 @@ module.exports = {
         "no-plusplus": 0,
         "quote-props": 0,
         "react/jsx-filename-extension": 0,
+        "jsx-a11y/anchor-is-valid": ["error", {
+            components: ["Link"],
+            specialLink: ["to"],
+            aspects: ["noHref", "invalidHref", "preferButton"],
+        }],
     },
 };
