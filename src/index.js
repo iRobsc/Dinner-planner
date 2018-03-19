7d91132a371d8c732e4d91e22cf368dc1e18a624
Willy Liu <arkazain@gmail.com>
2018-03-19 19:32:31 +0100

    🚧 Import basic CSS in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import "./css/index.scss";
+import "./css/responsive.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
