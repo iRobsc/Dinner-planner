124eb4004a940e1ce716f540ac770fff5325af0c
Willy Liu <arkazain@gmail.com>
2018-03-20 13:41:40 +0100

    🚧 Implement sidebar toggle and update formatting

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,59 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import DinnerButtonLink from "./DinnerButtonLink";
 import SidebarItem from "./SidebarItem";
+import "../css/Sidebar.scss";
 
-function Sidebar({ numberOfGuests, menu, onGuestChange }) {
-  let content;
-  if (menu.length === 0) {
-    content = "Your list is empty!";
-  } else {
-    content = menu.map(dish => <SidebarItem title={dish.title} price={dish.price} />);
+class Sidebar extends Component {
+  state = {
+    open: false,
+  };
+
+  toggleSidebar = () => {
+    this.setState({ open: !this.state.open });
   }
 
-  let disabled = false;
+  render() {
+    const { numberOfGuests, menu, onGuestChange } = this.props;
+    const { open } = this.state;
 
-  if (numberOfGuests === 0 || menu.length === 0) {
-    disabled = true;
-  }
+    let content;
+    if (menu.length === 0) {
+      content = "Your list is empty!";
+    } else {
+      content = menu.map(dish => <SidebarItem title={dish.title} price={dish.price} />);
+    }
 
-  return (
-    <div id="sidebar-container">
-      <div id="sidebar">
-        <div id="sidebar-accordion">
-          <h2>My Dinner</h2>
-          <div className="price">0kr</div>
-          <button id="sidebar-toggle">
-            <i className="fa fa-bars" />
-          </button>
-        </div>
-        <div id="sidebar-content" className="sidebar-hide">
-          <h2>My Dinner</h2>
+    let disabled = false;
+    if (numberOfGuests === 0 || menu.length === 0) {
+      disabled = true;
+    }
 
-          <div id="sidebar-inner-container">
-            Number of people:
-            <input
-              value={numberOfGuests}
-              onChange={onGuestChange}
-              id="number-of-guests"
-              type="number"
-            />
-            <br />
+    return (
+      <div id="sidebar-container">
+        <div id="sidebar">
+          <div id="sidebar-accordion">
+            <h2>My Dinner</h2>
+            <div className="price">0kr</div>
+            <button id="sidebar-toggle" onClick={this.toggleSidebar}>
+              <i className="fa fa-bars" />
+            </button>
+          </div>
+          <div id="sidebar-content" className={open ? "sidebar-show" : "sidebar-hide"}>
+            <h2>My Dinner</h2>
 
-            <div id="sidebar-list">{content}</div>
+            <div id="sidebar-inner-container">
+              Number of people:{" "}
+              <input
+                value={numberOfGuests}
+                onChange={onGuestChange}
+                id="number-of-guests"
+                type="number"
+              />
+              <br />
 
-            Total cost:
-            <span className="price">0kr</span>
-            <br />
-            <DinnerButtonLink to="/mydinner" disabled={disabled}>
-              Confirm Dinner
-              <i className="fa fa-check-circle" />
-            </DinnerButtonLink>
+              <div id="sidebar-list">{content}</div>
+
+              Total cost: <span className="price">0kr</span>
+              <br />
+              <DinnerButtonLink to="/mydinner" disabled={disabled}>
+                Confirm Dinner <i className="fa fa-check-circle" />
+              </DinnerButtonLink>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Sidebar.propTypes = {
