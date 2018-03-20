6cba5475316777bd813abd5094a8aaafab456ede
Willy Liu <arkazain@gmail.com>
2018-03-20 13:50:11 +0100

    🚧 Add state in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AppRoute from "../pages/AppRoute";
+import MainLayout from "../layouts/MainLayout";
 import Welcome from "../components/Welcome";
 import SearchPage from "../pages/SearchPage";
 import DishDetailsPage from "../pages/DishDetailsPage";
@@ -8,12 +10,28 @@ import RecipesPage from "../pages/RecipesPage";
 import NoMatchPage from "../pages/NoMatchPage";
 
 class App extends Component {
+  state = {
+    menu: [],
+    numberOfGuests: 0,
+  };
+
+  setNumberOfGuests = (numberOfGuests) => {
+    this.setState({ numberOfGuests });
+  }
+
   render() {
+    const { numberOfGuests, menu } = this.state;
+    const { setNumberOfGuests } = this;
     return (
       <Router>
         <Switch>
           <Route exact path="/" component={Welcome} />
-          <Route path="/search/:type/:keywords/:page" component={SearchPage} />
+          <AppRoute
+            path="/search/:type?/:keywords?/:page?"
+            component={SearchPage}
+            layout={MainLayout}
+            componentProps={{ numberOfGuests, menu, setNumberOfGuests }}
+          />
           <Route path="/dish/:id" component={DishDetailsPage} />
           <Route path="/mydinner" component={MyDinnerPage} />
           <Route path="/recipes" component={RecipesPage} />
