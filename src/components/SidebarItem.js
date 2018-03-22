0c2e5dea2b74a74a9f4fb3af0321f34bed219c74
Willy Liu <arkazain@gmail.com>
2018-03-22 23:24:35 +0100

    🚧 Extract SidebarItem.scss

--- src/components/SidebarItem.js
+++ src/components/SidebarItem.js
@@ -1,13 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "../css/SidebarItem.scss";
 
 function SidebarItem({ title, price }) {
   return (
-    <div>
-      <div className="sidebar-item">
-        <div>{title}</div>
-        <div>{Math.round(price)} SEK</div>
-      </div>
+    <div className="sidebar-item">
+      <div className="name">{title}</div>
+      <div className="price">{Math.round(price)} SEK</div>
+      <button className="delete">
+        <span className="fa-layers fa-fw">
+          <i className="fas fa-circle" />
+          <i className="fa-inverse fas fa-times" data-fa-transform="shrink-6" />
+        </span>
+      </button>
     </div>
   );
 }
